76482d8549700222eac662ce64acf89b1c79dbea
Vimal Mollyn <ms123vimal@gmail.com>
2022-01-14 10:37:10 -0500

    Changed colorscheme + added omnisharp

---
+++ lua/user/lsp/settings/omnisharp.lua
@@ -0,0 +1,11 @@
+local nvim_lsp = require'lspconfig'
+
+local pid = vim.fn.getpid()
+-- On linux/darwin if using a release build, otherwise under scripts/OmniSharp(.Core)(.cmd)
+local omnisharp_bin = "/opt/homebrew/bin/omnisharp"
+
+return {
+  cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) },
+  root_dir = nvim_lsp.util.root_pattern("*.csproj","*.sln"),
+}
+
